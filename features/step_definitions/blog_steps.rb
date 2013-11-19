d8909ac7941daed2d6d2d4c54227e2d67a6b9860
Sebastian Scandolo <seba.as@gmail.com>
2013-11-19 17:56:48 -0300

    Step 3

---
+++ features/step_definitions/blog_steps.rb
@@ -0,0 +1,15 @@
+Given(/^I am at the home page$/) do
+  visit "/"
+end
+
+When(/^I press "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I fill the "(.*?)" with "(.*?)"$/) do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
